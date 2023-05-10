adf33dda785fd5c7f5015b3c5cc7435c1d17f619
Gukuy <iliahide@gmail.com>
2023-05-10 23:43:21 +0300

    Четвёртое задание полностью готово еще раз

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,34 +1,34 @@
 function Student(name, gender, age) {
 
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
-  }
-  
-  Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
-  }
-  
-  Student.prototype.addMarks = function (...marks) {
-  
-  if (!this.marks) {
-        console.log("Студент исключен из учебного процесса");
-        return;
-  }
-    this.marks.push(...marks);
-    }
-  
-  Student.prototype.getAverage = function()  {
-       if (!this.marks || this.marks.length === 0) {
-      return 0
-  } 
-  return this.marks.reduce((acc, currentEstimate) => acc + currentEstimate, 0) / this.marks.length;
-  }
-  
-  Student.prototype.exclude = function (reason) {
-  
-    delete this.subject;
-    delete this.marks;
-    this.excluded = reason;
-  }
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+}
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+}
+
+Student.prototype.addMarks = function(...marks) {
+
+	if (!this.marks) {
+		console.log("Студент исключен из учебного процесса");
+		return;
+	}
+	this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) {
+		return 0
+	}
+	return this.marks.reduce((acc, currentEstimate) => acc + currentEstimate, 0) / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
+}
